f24d9512974173923e882f5e034c141d0f88e0e9
s3virge <kobzarvit@gmail.com>
2019-03-29 07:41:14 +0200

    first commit

---
+++ src/main/java/Employee.java
@@ -0,0 +1,7 @@
+public class Employee {
+    private int id;
+    private String name;
+    private float salary;
+//no-arg and parameterized constructors
+//getters and setters
+}
---
+++ src/main/java/EmployeeDao.java
@@ -0,0 +1,17 @@
+import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+
+public class EmployeeDao {
+    SimpleJdbcTemplate template;
+
+    public EmpDao(SimpleJdbcTemplate template) {
+        this.template = template;
+    }
+    public int update (Emp e){
+        String query="update employee set name=? where id=?";
+        return template.update(query,e.getName(),e.getId());
+
+//String query="update employee set name=?,salary=? where id=?";  
+//return template.update(query,e.getName(),e.getSalary(),e.getId());  
+    }
+
+}  
